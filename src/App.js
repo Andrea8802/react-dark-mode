1fb5a5e779960175b8f0481ccca2ca0f37f7b8ab
Andrea Bonaventura <andreabonaventura802@gmail.com>
2023-03-29 12:58:08 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import data from "./data";
+import Articolo from "./Articolo";
+
+
+const getThemeFromStorage = () => {
+  return localStorage.getItem("theme") ?? "light-mode";
+
+}
+
 
 function App() {
+
+  const [theme, setTheme] = useState(getThemeFromStorage());
+
+  const switchTheme = () => {
+    if (theme === "light-mode") {
+      setTheme("dark-mode")
+    } else {
+      setTheme("light-mode")
+    }
+  }
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem("theme", theme);
+
+  }, [theme])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <div className="container">
+        <h2>DARK MODE APP</h2>
+
+        <button className="btn" onClick={switchTheme}>
+          Switch Theme
+        </button>
+
+        <section className="article-section">
+          {data.map(el => <Articolo key={el.id} {...el} />)}
+
+        </section>
+
+      </div>
+    </section>
   );
 }
 
